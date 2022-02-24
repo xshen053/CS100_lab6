c33262be9176f624c20bc8f5ccd2a50925b61d97
BFLion <hyu146@ucr.edu>
2022-02-23 19:52:35 -0800

    cmake

---
+++ CMakeFiles/spreadsheet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spreadsheet.dir/main.cpp.o"
+  "CMakeFiles/spreadsheet.dir/spreadsheet.cpp.o"
+  "spreadsheet"
+  "spreadsheet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spreadsheet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
